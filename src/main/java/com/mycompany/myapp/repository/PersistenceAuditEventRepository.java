82660bcc017b40c7dd045c7eabf8be2c82916c27
cbornet <cbornet@hotmail.com>
2019-12-23 13:57:59 +0100

    Initial version of r2dbc generated by JHipster-6.6.0

---
+++ src/main/java/com/mycompany/myapp/repository/PersistenceAuditEventRepository.java
@@ -0,0 +1,23 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.PersistentAuditEvent;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+
+/**
+ * Spring Data JPA repository for the {@link PersistentAuditEvent} entity.
+ */
+public interface PersistenceAuditEventRepository extends ReactiveJpaRepository<PersistentAuditEvent, Long> {
+
+    Flux<PersistentAuditEvent> findAllByAuditEventDateBetween(Instant fromDate, Instant toDate, Pageable pageable);
+
+    Flux<PersistentAuditEvent> findByAuditEventDateBefore(Instant before);
+    
+    Flux<PersistentAuditEvent> findAllBy(Pageable pageable);
+
+    Mono<Long> countByAuditEventDateBetween(Instant fromDate, Instant toDate);
+}
---
+++ src/main/java/com/mycompany/myapp/repository/UserRepository.java
@@ -0,0 +1,43 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.User;
+
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+
+/**
+ * Spring Data JPA repository for the {@link User} entity.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>Reactive {
+
+    Mono<User> findOneByActivationKey(String activationKey);
+
+    Flux<User> findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(Instant dateTime);
+
+    Mono<User> findOneByResetKey(String resetKey);
+
+    Mono<User> findOneByEmailIgnoreCase(String email);
+
+    Optional<User> findOneByLogin(String login);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesById(Long id);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByLogin(String login);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByEmailIgnoreCase(String email);
+
+    Flux<User> findAllByLoginNot(Pageable pageable, String login);
+
+    Mono<Long> countAllByLoginNot(String anonymousUser);
+}
